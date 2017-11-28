9c67b41ce9bf5635c399dd3050acce73bf2fe5ba
xsec76 <34009975+xsec76@users.noreply.github.com>
2017-11-27 21:31:03 -0500

    Create foundation for command-line argument parsing and outline application logic
    Base functionality of command-line argument parsing and outline of application logic.

--- matchvid/matchvid.py
+++ matchvid/matchvid.py
@@ -1,5 +1,24 @@
-import sys
+import argparse
 import numpy
+from .keyframeops import KeyframeOps
 
 def main():
-    print("Project structure.")
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('videos', type = str, nargs='+', help='First video is matched against other videos provided.')
+    parser.add_argument('--start', type=int, help='Start time of video in seconds.')
+    parser.add_argument('--end', type=int, help='End time of video in seconds.')
+    args = parser.parse_args() # need to add interval selection
+    primary_keyframe_list = KeyframeOps.get_keyframes(arg.videos[0])
+    '''
+    Carry out wavelt transform on each frame
+    '''
+    for video in args.videos[1:]:
+        secondary_keyframe_list = KeyframeOps.get_keyframes(video)
+        '''
+        Carry out wavelet transform on each video
+        Compare transfrom between primary and secondary videos
+        Record locations of matching frames in time
+        '''
+    '''
+    Print location of matches in time of secondary videos
+    '''
