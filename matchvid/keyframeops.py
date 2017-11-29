11efba44b71ae7e29d749e79deba2e5ff0ef26a7
xsec76 <34009975+xsec76@users.noreply.github.com>
2017-11-29 01:23:12 -0500

    Add ability to capture time of frame during keyframe extraction
    The fps of the video undergoing keyframe extraction is determined and used to calculate the time (in seconds) that the frame appears in the video.

--- matchvid/keyframeops.py
+++ matchvid/keyframeops.py
@@ -11,18 +11,20 @@ class KeyframeOps(object):
     def get_keyframes(video_path):
         keyframe_list = []
         p_frame_thresh = 300000 # This may need to be adjusted
-        cap = cv2.VideoCapture(video_path)
-        success, previous_frame = cap.read() # Read the first frame.
-        count = 0
+        video = cv2.VideoCapture(video_path)
+        video_fps = video.get(cv2.CAP_PROP_FPS) # Get the frame rate
+        success, previous_frame = video.read() # Read the first frame
+        frame_number = 1
         while success:
-            success, current_frame = cap.read()
+            success, current_frame = video.read() # -> Returns (boolean, image Array)
             if success:
                 diff = cv2.absdiff(current_frame, previous_frame)
                 non_zero_count = np.count_nonzero(diff)
                 if non_zero_count > p_frame_thresh:
                     # Convert images to 8-Bit grayscale
                     image = cv2.cvtColor(previous_frame, cv2.COLOR_BGR2GRAY)
-                    keyframe_list.append(image)
+                    # Appends (keyframe, time of frame in seconds)
+                    keyframe_list.append((image, frame_number / video_fps))
                 previous_frame = current_frame
-                count += 1
+                frame_number += 1
         return keyframe_list
